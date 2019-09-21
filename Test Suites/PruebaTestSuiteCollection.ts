e0c300478065de474cf6915e0b89ac181a30f821
mcoronado <matias.coronado@usach.cl>
2019-09-20 23:22:28 -0300

    Test 5

--- Test Suites/PruebaTestSuiteCollection.ts
+++ Test Suites/PruebaTestSuiteCollection.ts
@@ -5,5 +5,15 @@
    <tag></tag>
    <executionMode>SEQUENTIAL</executionMode>
    <maxConcurrentInstances>8</maxConcurrentInstances>
-   <testSuiteRunConfigurations/>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <runConfigurationId>Chrome (headless)</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/PruebaTestSuites</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
 </TestSuiteCollectionEntity>
