67260897d4f115ff9767bd39a8b9832453ef2c8a
mcoronado <matias.coronado@usach.cl>
2019-09-20 23:21:18 -0300

    Test 4

--- Test Suites/PruebaTestSuites.ts
+++ Test Suites/PruebaTestSuites.ts
@@ -11,7 +11,7 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>b3241d48-fa7f-43fe-a583-5a74ccc65f08</testSuiteGuid>
    <testCaseLink>
-      <guid>88701592-7e45-4d55-94d9-cc0cdc2dd56a</guid>
+      <guid>cd21a264-b3f8-4f9e-b501-0885708bc8a6</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TestA</testCaseId>
