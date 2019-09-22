5a7373375160e3559ff706f09f1e73ea9ab73af2
mcoronado <matias.coronado@usach.cl>
2019-09-21 21:21:07 -0300

    fff

--- Test Suites/PruebaTestSuiteCollection.ts
+++ Test Suites/PruebaTestSuiteCollection.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteCollectionEntity>
    <description></description>
-   <name>TestSuiteCollection</name>
+   <name>PruebaTestSuiteCollection</name>
    <tag></tag>
    <executionMode>SEQUENTIAL</executionMode>
    <maxConcurrentInstances>8</maxConcurrentInstances>
